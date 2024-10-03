d46f9c37468307448403ae428fb7b510bae0aa19
MatiasAhumada <mahumada486@gmail.com>
2024-10-03 09:57:59 -0300

    clase 3/10

---
+++ Proyecto/JS/index.js
@@ -0,0 +1,59 @@
+//Variables
+var var1 = 1;
+const con1 = 1;
+let let1 = 11;
+//Tipos de datos
+/* bool
+int
+float
+string
+char
+number
+array
+object
+*/
+//Operadores
+// asignacion
+// aritmeticos
+// unitarios(++,--,!)
+// condicionales
+// relacionales
+// logicos (&&,||)
+//&&=Y
+//||=O
+let array = [1, 2, 3, 4, 6];
+let suma = 0;
+//bucles de repeticion
+//WHILE
+//FOR
+// for (let i = 0; i < array.length; i++) {
+//   array[3]=2
+// }
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+// console.log(suma);
+//  while (array.length > c) {
+//    console.log(array[c]);
+//    c++;
+//  }
+
+// do{
+//     console.log(array[c]);
+//     c++
+// }while(array.length > c);
+
+//FUNCIONES
+
+function primerF(i,g,j){
+    // for (let i = 0; i < array.length; i++) {
+    //     array[3]=2
+    //   }
+    //   for (let i = 0; i < array.length; i++) {
+    //     console.log(array[i]);
+    //   }
+    console.log(i+g+j);
+}
+primerF("Hola", " ","Mundo");
+
+
