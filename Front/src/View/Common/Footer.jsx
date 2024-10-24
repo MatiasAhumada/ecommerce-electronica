cfac70c6bf55cb566ba55646bbe2fbd46f72b48f
MatiasAhumada <mahumada486@gmail.com>
2024-10-24 09:59:24 -0300

    Inicialice programa react con vite y aplique formato en toda la app

---
+++ Front/src/View/Common/Footer.jsx
@@ -0,0 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Row>
+        <Col>
+          <h3>izquierda</h3>
+        </Col>
+        <Col>
+          <h3> derecha</h3>
+        </Col>
+      </Row>
+    </footer>
+  );
+};
+
+export default Footer;
