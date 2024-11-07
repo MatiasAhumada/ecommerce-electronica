9e67195f3055995a7b641df75401e74c965ed1a9
Jose <joseluis_sde@hotmail.com>
2024-11-07 10:33:41 -0300

    clase7/11

--- Front/src/View/Common/Navbar.jsx
+++ Front/src/View/Common/Navbar.jsx
@@ -1,24 +1,28 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Container, Nav, Navbar, Row, Col } from "react-bootstrap";
 
 const Navb = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+  return (
+    <Navbar expand="lg" className="bg-body-dark">
       <Container>
-        <Navbar.Brand href="#home">Nombre App</Navbar.Brand>
+        <Navbar.Brand  href="#home">Nombre App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Iniciar Sesion</Nav.Link>
-            <Nav.Link href="#link">Regitrate</Nav.Link>
+          <Nav className="me-auto w-100">
+            <Row className="w-100"> 
             
+              <Col md={9}></Col>
+              <Col md={2} xs={12}>
+                <Nav.Link href="#home">Iniciar Sesion</Nav.Link>
+              </Col>
+              <Col md={1} xs={12}>
+                <Nav.Link href="#link">Regitrate</Nav.Link>
+              </Col>
+            </Row>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 };
 
 export default Navb;
