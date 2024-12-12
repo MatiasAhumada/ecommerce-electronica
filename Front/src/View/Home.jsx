9ecb5871ca8f833f06add63fa83b436a18520817
Patricio <nathanielcountact@gmail.com>
2024-12-12 10:18:03 -0300

    comentario

--- Front/src/View/Home.jsx
+++ Front/src/View/Home.jsx
@@ -1,11 +1,25 @@
-import {Container} from "react-bootstrap"
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
-const Home = () => {
-    return (
-        <Container>
-            <h3>¿Quienes Somos?</h3>
-        </Container>
-    );
+ const Home = () => {
+  return (
+    <Container>
+        <Row>
+           <Col lg={3} md={6} sm={12} key={objeto.id}>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+           </Col>
+        </Row>
+    </Container>
+  );
 };
 
 export default Home;
