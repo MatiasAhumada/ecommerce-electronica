b27fe0dbaac35effbfe423fde8b073876221eaf1
MatiasAhumada <pagosptcmatias@gmail.com>
2024-12-12 10:17:22 -0300

    clase 12/12

--- Front/src/View/Home.jsx
+++ Front/src/View/Home.jsx
@@ -1,11 +1,42 @@
-import {Container} from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Button, Card, Container, Row, Col } from "react-bootstrap";
+import { consulta } from "../service/queries";
 
 const Home = () => {
-    return (
-        <Container>
-            <h3>¿Quienes Somos?</h3>
-        </Container>
-    );
+  const [dataObj, setDataObj] = useState([]);
+
+  useEffect(() => {
+    try {
+      consulta().then((res) => {
+        setDataObj(res);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const mapeo = () => {
+    return dataObj.map((r) => (
+      <Col key={r.id} lg={4} md={6} sm={12}>
+        <Card style={{ width: "18rem" }} className="mt-3 text-center">
+          <Card.Img variant="top" src={r.image} />
+          <Card.Body>
+            <Card.Title>{r.name}</Card.Title>
+            <Card.Subtitle className="mt-1">{r.species}</Card.Subtitle>
+            <Card.Subtitle className="mt-1">{r.status}</Card.Subtitle>
+            <Card.Subtitle className="mt-1">{r.location.name}</Card.Subtitle>
+            <Button variant="primary" className="mt-2">Ver Mas</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    ));
+  };
+
+  return (
+    <Container>
+      <Row>{mapeo()}</Row>
+    </Container>
+  );
 };
 
 export default Home;
---
+++ Front/src/service/queries.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const URL = "https://rickandmortyapi.com/api/character";
+
+export const consulta = async () => {
+  try {
+    const respuesta = await axios.get(URL);
+    return respuesta.data.results;
+  } catch (error) {
+    console.log(error);
+  }
+};
