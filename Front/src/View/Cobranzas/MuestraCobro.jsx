86dfed7bf163f3084b8daadb8f6a92721909ddb4
MatiasAhumada <pagosptcmatias@gmail.com>
2025-01-02 09:40:39 -0300

    terminamos la pagina de mostrar cobro

--- Front/src/View/Cobranzas/MuestraCobro.jsx
+++ Front/src/View/Cobranzas/MuestraCobro.jsx
@@ -1,88 +1,108 @@
 import React from "react";
-import { Button, Container, Form, InputGroup } from "react-bootstrap";
+import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const MuestraCobro = () => {
   return (
-    <>
-      <Container className="d-flex justify-content-center mt-5 mb-5">
-        <Form className="text-center w-50" onSubmit={handleSubmit}>
+    <Container className="text-center mt-5 mb-5">
+      <Row>
+        <Col>
           <Form.Group className="mb-3" controlId="cliente">
             <Form.Label>Nombre y Apellido</Form.Label>
-            <Form.Control type="text" placeholder="Pedro Juarez" />
+
+            <Form.Control
+              type="text"
+              disabled
+              value=""
+              placeholder="Pedro Juarez"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="cliente">
             <Form.Label>Contacto o Email</Form.Label>
-            <Form.Control type="text" placeholder="3813528658" />
+            <Form.Control
+              type="text"
+              disabled
+              value=""
+              placeholder="3813528658"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupEmail">
             <Form.Label>DNI Cliente</Form.Label>
-            <Form.Control type="number" placeholder="Ej:43026159" />
+            <Form.Control
+              type="number"
+              disabled
+              value=""
+              placeholder="Ej:43026159"
+            />
           </Form.Group>
-        </Form>
-      </Container>
-      <Container className="d-flex justify-content-center mt-5 mb-5">
-        <Form.Group className="mb-3" controlId="Marca">
-          <Form.Label>Marca</Form.Label>
-          <Form.Control type="text" placeholder="Samsung" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="Modelo">
-          <Form.Label>Modelo</Form.Label>
-          <Form.Control type="text" placeholder="A54" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="falla">
-          <Form.Label>Falla</Form.Label>
-          <Form.Control type="text" placeholder="Cambio pin de carga" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="descripcion">
-          <Form.Label>Descripcion de falla</Form.Label>
-          <Form.Control as="textarea" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="presupuesto dado">
-          <Form.Label>Presupuesto Dado</Form.Label>
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
-            <Form.Control type="number" aria-label="presupuesto" />
-          </InputGroup>
-        </Form.Group>
-      </Container>
-      <Container className="mb-5 mt-3 text-center w-50">
-        <Form>
-          <Form.Group className="mt-3">
-            <Form.Label>Nombre del Equipo</Form.Label>
-            <Form.Select>
-              <option>Equipos</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </Form.Select>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3" controlId="Marca">
+            <Form.Label>Marca</Form.Label>
+            <Form.Control type="text" disabled value="" placeholder="Samsung" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Group className="mb-3" controlId="Modelo">
+            <Form.Label>Modelo</Form.Label>
+            <Form.Control type="text" disabled value="" placeholder="A54" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="falla">
+            <Form.Label>Falla</Form.Label>
+            <Form.Control
+              type="text"
+              disabled
+              value=""
+              placeholder="Cambio pin de carga"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="descripcion">
+            <Form.Label>Descripcion de falla</Form.Label>
+            <Form.Control as="textarea" disabled value="" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="presupuesto dado">
+            <Form.Label>Presupuesto Dado</Form.Label>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
+              <Form.Control
+                type="number"
+                placeholder="1111"
+                disabled
+                value=""
+              />
+            </InputGroup>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-3" controlId="Monto a Cobrar">
             <Form.Label>Monto a Cobrar</Form.Label>
-            <Form.Control type="number" placeholder="Ej:$100.000" />
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
+              <Form.Control
+                type="number"
+                placeholder="1111"
+                disabled
+                value=""
+              />
+            </InputGroup>
           </Form.Group>
           <Form.Group className="mt-3">
             <Form.Label>Metodos de pagos</Form.Label>
-            <Form.Select>
-              <option>Metodos de pagos</option>
-              <option value="Mercado Pago">Mercado Pago</option>
-              <option value="Credito/Debito">Credito/Debito</option>
-              <option value="Transferencia">Transferencia</option>
-              <option value="QR">QR</option>
-            </Form.Select>
+            <Form.Control
+              type="text"
+              placeholder="MercadoPago"
+              disabled
+              value=""
+            />
           </Form.Group>
-        </Form>
-        <Form.Group className="mb-3" controlId="Monto a Cobrar">
-          <Form.Label>Monto a Cobrar</Form.Label>
-          <InputGroup className="mb-3">
-            <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
-            <Form.Control type="number" aria-label="presupuesto" />
-          </InputGroup>
-        </Form.Group>
-        <Button as={Link} to="/muestraCobro">
-          Cobrar
-        </Button>
-      </Container>
-    </>
+          <Form.Group className="mb-3" controlId="falla">
+            <Form.Label>Garantia</Form.Label>
+            <Form.Control type="text" disabled value="" placeholder="1 año" />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Button as={Link} type="submi">
+        Imprimir Comprobante
+      </Button>
+    </Container>
   );
 };
 
--- Front/src/service/queries.js
+++ Front/src/service/queries.js
@@ -3,7 +3,7 @@ const URL = "https://rickandmortyapi.com/api/character";
 
 export const consulta = async () => {
   try {
-    const respuesta = await axios.get(URL);
+    const respuesta =  await axios.get(URL);
     return respuesta.data.results;
   } catch (error) {
     console.log(error);
